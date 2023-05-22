a3387499afaa90fe2f7a35b0fd1c777cc070d58d
Tanaya <desaitanaya17@gmail.com>
2023-05-22 19:55:47 +0530

    [Tanaya] Add : C# Program to display the Prime numbers in a given range.

---
+++ PrimeNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace PrimeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrimeNumber primeNumber = new PrimeNumber();
+
+            int size;
+
+            Console.WriteLine("Enter the size of array :");
+            size = int.Parse(Console.ReadLine());
+
+            //Array declaration
+            int[] array = new int[size];
+
+            
+
+            Console.WriteLine("PRIME NUMBERS");
+            primeNumber.PrimeNumberMethod(size, array);
+        }
+    }
+}
