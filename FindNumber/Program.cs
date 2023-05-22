85d88139fd84b5f3af77a07332b04e1dac80e239
Tanaya <desaitanaya17@gmail.com>
2023-05-22 19:54:00 +0530

    [Tanaya] Add : C# Program to find the number guessed by the user.

---
+++ FindNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace FindNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the limit: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int left= 0;
+            int right = (int)Math.Pow(2, (n-1));
+
+            Console.WriteLine("Think of a number between 0 and " + right);
+
+            FindNumber findNumber = new FindNumber();
+            Console.WriteLine("The number is: ");
+            findNumber.FindNumberMethod(left, right);
+
+           
+        }
+    }
+}
