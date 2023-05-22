2e647bacf9ed61fae2b48d5f7c31504ed62cc4da
Tanaya <desaitanaya17@gmail.com>
2023-05-22 19:51:21 +0530

    [Tanaya] Add : C# Program to check for Anagram in String.

---
+++ AnagramDetection/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace AnagramDetection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Anagram anagramDetection = new Anagram();
+
+            string firstString, secondString;
+
+            Console.WriteLine("Enter the First String:");
+            firstString = Console.ReadLine();
+
+            Console.WriteLine("Enter the Second String:");
+            secondString = Console.ReadLine();
+
+            anagramDetection.AnagramDetectionMethod(firstString, secondString);
+
+        }
+    }
+}
