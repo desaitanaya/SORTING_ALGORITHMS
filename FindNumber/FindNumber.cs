107d7b952b8408814ca66bd8a5abbb73392cc7ef
Tanaya <desaitanaya17@gmail.com>
2023-05-22 19:54:07 +0530

    [Tanaya] Add : C# Program to find the number guessed by the user.

---
+++ FindNumber/FindNumber.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindNumber
+{
+    internal class FindNumber
+    {
+        public void FindNumberMethod(int left, int right)
+        {
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+
+                Console.WriteLine("Is the number equal to, greater than, or less than " + mid);
+                Console.WriteLine("Enter 'equal', 'greater' or 'less'");
+                string userInput = Console.ReadLine();
+
+                if (userInput == "equal")
+                {
+                    Console.WriteLine("The number is: " +mid);
+                    break;
+                }
+                else if (userInput == "greater")
+                {
+                    left = mid + 1;
+                }
+                else if (userInput == "less")
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter 'equal', 'greater', or 'less'.");
+                }
+            }
+
+        }
+    }
+}
